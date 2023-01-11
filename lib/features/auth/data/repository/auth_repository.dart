a251032107a7a83074417e41373c08ca81f096a3
Adole Samuel Sani <adolesamuel4@gmail.com>
2023-01-11 13:29:29 +0100

    updating database layers

--- lib/features/auth/data/repository/auth_repository.dart
+++ lib/features/auth/data/repository/auth_repository.dart
@@ -1,39 +1,32 @@
 import 'package:dartz/dartz.dart';
 import 'package:dukka_finance/core/failures/failure.dart';
 import 'package:dukka_finance/features/auth/data/models/auth_signin.dart';
-import 'package:dukka_finance/features/database/database.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-final firebaseAuthProvider =
+final _firebaseAuthProvider =
     Provider<FirebaseAuth>((ref) => FirebaseAuth.instance);
 
-// 2
 final authStateChangesProvider = StreamProvider<User?>(
-    (ref) => ref.watch(firebaseAuthProvider).authStateChanges());
+    (ref) => ref.watch(_firebaseAuthProvider).authStateChanges());
 
-final databaseProvider = Provider<FirestoreDatabase?>((ref) {
-  final auth = ref.watch(authStateChangesProvider);
-
-  // we only have a valid DB if the user is signed in
-  if (auth.value?.uid != null) {
-    return FirestoreDatabase(uid: auth.value!.uid);
-  }
-  // else we return null
-  return null;
-});
+final authRepoProvider = Provider<AuthRepository>(
+  (ref) => AuthRepositoryImpl(ref),
+);
 
 abstract class AuthRepository {
   Future<Either<Failure, UserCredential>> createUserWithEmailAndPassword(
       AuthSignIn data);
   Future<Either<Failure, UserCredential>> signInUserWithEmailAndPassword(
       AuthSignIn data);
+
+  Stream<User?> authStream();
 }
 
 class AuthRepositoryImpl implements AuthRepository {
   final FirebaseAuth _firebaseAuth;
 
-  AuthRepositoryImpl(Ref ref) : _firebaseAuth = ref.read(firebaseAuthProvider);
+  AuthRepositoryImpl(Ref ref) : _firebaseAuth = ref.read(_firebaseAuthProvider);
 
   @override
   Future<Either<Failure, UserCredential>> createUserWithEmailAndPassword(
@@ -57,5 +50,10 @@ class AuthRepositoryImpl implements AuthRepository {
       final failure = CommonFailure('SignIn Failure', error.toString());
       throw Left(failure);
     });
+  }
+
+  @override
+  Stream<User?> authStream() async* {
+    yield* _firebaseAuth.authStateChanges();
   }
 }
--- lib/features/database/database.dart
+++ lib/features/database/database.dart
@@ -1,9 +1,26 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:dukka_finance/features/auth/data/repository/auth_repository.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 final firebaseProvider = Provider((ref) => FirebaseFirestore.instance);
 
+final databaseProvider = Provider<FirestoreDatabase?>((ref) {
+  final auth = ref.watch(authStateChangesProvider);
+  final dat = ref.read(firebaseProvider);
+
+  // we only have a valid DB if the user is signed in
+  if (auth.value?.uid != null) {
+    return FirestoreDatabase(uid: auth.value!.uid, firestoreDatabase: dat);
+  }
+  // else we return null
+  return null;
+});
+
 class FirestoreDatabase {
-  FirestoreDatabase({required this.uid}) : assert(uid != null);
+  FirestoreDatabase({
+    required this.uid,
+    required this.firestoreDatabase,
+  });
   final String uid;
+  final FirebaseFirestore firestoreDatabase;
 }

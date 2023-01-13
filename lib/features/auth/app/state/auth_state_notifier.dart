10697fa74080c52268016a83f02615793a870b51
Adole Samuel Sani <adolesamuel4@gmail.com>
2023-01-13 12:39:30 +0100

    auth state complete

--- lib/features/auth/app/state/auth_state_notifier.dart
+++ lib/features/auth/app/state/auth_state_notifier.dart
@@ -1,4 +1,5 @@
 import 'package:dukka_finance/features/auth/app/state/auth_state.dart';
+import 'package:dukka_finance/features/auth/data/models/auth_signin.dart';
 import 'package:dukka_finance/features/auth/data/repository/auth_repository.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -13,4 +14,33 @@ class AuthStateNotifier extends StateNotifier<AuthState> {
         super(AuthStateInitial());
 
   Stream<User?> authStateChanges() => _authRepository.authStream();
+
+  Future<void> login({required String email, required String password}) async {
+    state = LogInLoadingState();
+
+    final AuthSignIn loginItem = AuthSignIn(email, password);
+
+    final logInOrError =
+        await _authRepository.signInUserWithEmailAndPassword(loginItem);
+
+    state = logInOrError.fold(
+      (l) => LogInFailureState(l),
+      (r) => LogInSuccessState(r),
+    );
+  }
+
+  Future<void> createPassword(
+      {required String email, required String password}) async {
+    state = CreatePasswordLoadingState();
+
+    final AuthSignIn loginItem = AuthSignIn(email, password);
+
+    final createPasswordOrError =
+        await _authRepository.createUserWithEmailAndPassword(loginItem);
+
+    state = createPasswordOrError.fold(
+      (l) => CreatePasswordFailureState(l),
+      (r) => CreatePasswordSuccessState(r),
+    );
+  }
 }

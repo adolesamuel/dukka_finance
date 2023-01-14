cf7fef71061d48324baf4dd4c2bd2b6f086ebfcd
Adole Samuel Sani <adolesamuel4@gmail.com>
2023-01-14 21:37:54 +0100

    centered title

--- lib/features/dashboard/app/dashboard.dart
+++ lib/features/dashboard/app/dashboard.dart
@@ -30,6 +30,7 @@ class _DashboardScreenState extends State<DashboardScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        centerTitle: true,
         leading: ProfileIcon(
           onPressed: () {
             _onItemTapped(2);

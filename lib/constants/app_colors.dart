3dad66214e40fd63afff96801bd52a814d75ffc3
Adole Samuel Sani <adolesamuel4@gmail.com>
2023-01-14 09:57:08 +0100

    added auth colors

--- lib/constants/app_colors.dart
+++ lib/constants/app_colors.dart
@@ -4,4 +4,8 @@ class AppColors {
   static const authScaffoldBgColor = Color(0xFF17223C);
   static const authAppBarBgColor = Color(0xFF17223C);
   static const authFontColor = Colors.white;
+  static const disabledBtnColor = Color(0xFF3C435A);
+  static const disableBtnFontColor = Color(0xFFC1C2C6);
+  static const snackbarErrorColor = Color(0xFFE14B58);
+  static const snackbarSuccessColor = Color(0xFF50B459);
 }
